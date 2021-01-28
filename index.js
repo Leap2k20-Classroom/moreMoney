274aba82a7abeb467c0249762495071d60f083ae
lhagvadarjaa <lhagva98@gmail.com>
2021-01-29 01:07:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+var gishuun = [
+  {
+    'name': 'Zoe',
+    'uria': 'Ta hedig ur huuhed shigee hairlana',
+    'money': 500,
+    'id:': 4,
+    "sanal_too": 1200,
+  },
+  {
+    'name': 'baatraa',
+    'uria': 'Hamtdaa edetsgeey',
+    'money': 900,
+    'id:': 1,
+    "sanal_too": 300,
+  },
+  {
+    'name': 'sodoo',
+    'uria': 'Huniih ni toog maliinh tootoi tentsuu bolgono',
+    'money': 1300,
+    'id:': 3,
+    "sanal_too": 300,
+  },
+  {
+    'name': 'dolgoon',
+    'uria': 'Namaig demjeerei tstserleg, yashlin anduuda!!!',
+    'money': 9999,
+    'id:': 2,
+    "sanal_too": 9999,
+  }
+
+]
+
+function search(maxPrecentage) {
+  //return "some gishuun id"; 
+}
+
+
+console.log(search(process.argv[2]))
