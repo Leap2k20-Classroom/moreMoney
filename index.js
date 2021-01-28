bdb3db8f19c27ae9e9a5a8515540d8163202253e
lhagvadarjaa <lhagva98@gmail.com>
2021-01-29 01:31:19 +0800

    bug-fix1

--- index.js
+++ index.js
@@ -3,54 +3,37 @@ var gishuuns = [
     'name': 'Zoe',
     'uria': 'Ta hedig ur huuhed shigee hairlana',
     'money': 500,
-    'id:': 4,
-    "sanal_too": 1200,
+    'id': 4,
+    "sanal_too": 600,
   },
   {
     'name': 'baatraa',
     'uria': 'Hamtdaa edetsgeey',
     'money': 900,
-    'id:': 1,
-    "sanal_too": 300,
+    'id': 1,
+    "sanal_too": 200,
   },
   {
     'name': 'sodoo',
     'uria': 'Huniih ni toog maliinh tootoi tentsuu bolgono',
-    'money': 1300,
-    'id:': 3,
-    "sanal_too": 300,
+    'money': 400,
+    'id': 3,
+    "sanal_too": 700,
   },
   {
     'name': 'dolgoon',
     'uria': 'Namaig demjeerei tstserleg, yashlin anduuda!!!',
-    'money': 9999,
-    'id:': 2,
-    "sanal_too": 9999,
+    'money': 999,
+    'id': 2,
+    "sanal_too": 101,
   }
 
 ]
 
 function search(maxPrecentage) {
 
-  var max = 0, gishuunId;
-
-  for(var i = 0; i < gishuuns.length; i ++) {
-    let tempMax = 0;
-    for(var j = 1; j <= maxPrecentage; j ++) {
-      var score = gishuuns[i].money * (j / 100) + gishuuns[i].sanal_too * ( (100 - j) / 100);
-      console.log(score)
-      if(tempMax < score ) { 
-        tempMax = score;
-      }
-    }
-    if(max < tempMax) { 
-      max = tempMax;
-      gishuunId = gishuuns[i].id;
-    }
-  }
-  return gishuunId;
   //return "some gishuun id"; 
 }
 
 
-console.log(search(process.argv[2]))
+console.log(search(process.argv[2]));
